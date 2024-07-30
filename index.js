7e6f59608c2aa174ee533a601379a5c3902c74d8
RaphaelGentils <raphael.gentils@oclock.school>
2024-07-30 13:50:59 +0200

    récupération intégration, création des vues et des partials, architecture express et ejs, création de la DB

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // variables d'environnement
 require('dotenv').config();
 
-// serveur
+// Sécurisation des chemins
+const path = require('path');
+
+// Serveur express
 const express = require('express');
-const port = process.env.PORT || 5000;
 
 const app = express();
+
+const router = require('./src/routers');
+
+// Configuration moteur de templates
+app.set('view engine', 'ejs');
+// Configuration répertoire views
+const securedPathToViews = path.join(__dirname, 'src/views');
+app.set('views', securedPathToViews);
+// configuration répertoire static
+const securedPathToAssets = path.join(__dirname, 'public');
+app.set(express.static(securedPathToAssets));
+
+app.use(router);
+
+// Last middleware 404
+const notFound = require('src/middlewares/404.js');
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
 	console.log(`Listening on ${process.env.BASE_URL}:${port}`);
---
+++ src/routers/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const mainController = require('../controllers/mainController');
+
+router.get('/', mainController.homePage);
+
+module.exports = router;
